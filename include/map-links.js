1a1dffa89234c9c479d5e7c69a92d157f8bd6e85
Martin Holmes <martin_j_holmes@hotmail.com>
2026-03-20 17:39:37 +0000

    More changes

--- include/map-links.js
+++ include/map-links.js
@@ -8,3 +8,4 @@ mapLinks += "<a class='custom-btn' href='portsaplaya.html'>Port Saplaya</a>";
 mapLinks += "<a class='custom-btn' href='barga.html'>Barga</a>";
 mapLinks += "<a class='custom-btn' href='test.html'>Test</a>";
 mapLinks += "<a class='custom-btn' href='test2.html'>Test 2</a>";
+mapLinks += "<a class='custom-btn' href='NewMap.html'>NewMap</a>";
